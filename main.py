12a646a30165e6411f5c5e34dbe5125d362b1e10
FeyXieXzf <52984443+FeyXieXzf@users.noreply.github.com>
2021-10-09 20:39:48 +0800

    Started port to kivy

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ from resc import *
 from math import floor
 from locale import getdefaultlocale
 
-import PyQt6.QtCore
-from PySide6.QtWidgets import *
-from PySide6.QtGui import *
-from PySide6.QtCore import *
-
 x = 0
 y = 0
 xP = 0
@@ -47,100 +42,9 @@ def calcPos(xN, yN, sN=0, calcT=True):
                     return False
 
 
-# noinspection PyUnresolvedReferences
-class SSGui(QWidget):
-    def __init__(self):
-        super(SSGui, self).__init__()
-        self.init_ui()
-
-    def init_ui(self):
-        global lang, sudoku
-        # Init layout
-        layout = QVBoxLayout()
-        self.setWindowTitle(r"*** Sudoku Solver GUI ***   |   By: Ujhhgtg")
-        self.resize(800, 740)
-        # Buttons!!!
-        # Run Button
-        self.btnRun = QPushButton(translates[lang]["btnRunName"], self)
-        self.btnRun.setFont(QFont("Noto Sans", 14))
-        self.btnRun.setCursor(QCursor(Qt.PointingHandCursor))
-        self.btnRun.clicked.connect(lambda: self.runSolver(0))
-        # Fast Run Button
-        self.btnFastRun = QPushButton(translates[lang]["btnFastModeName"], self)
-        self.btnFastRun.setFont(QFont("Noto Sans", 14))
-        self.btnFastRun.setCursor(QCursor(Qt.PointingHandCursor))
-        self.btnFastRun.clicked.connect(lambda: self.runSolver(1))
-        # Tables!!!
-        # Sudoku Table
-        self.tblSudoku = QTableWidget(9, 9)
-        self.tblSudoku.setFont(QFont("Noto Sans Arabic Light", 20))
-        self.tblSudoku.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.tblSudoku.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.tblSudoku.horizontalHeader().setVisible(False)
-        self.tblSudoku.verticalHeader().setVisible(False)
-        # Check Boxes!!!
-        # Fast Mode Check Box
-        self.chkBoxFast = QCheckBox()
-        self.chkBoxFast.setFont(QFont("Noto Sans", 14))
-        # Set Initial Sudoku
-        for ix in range(9):
-            for iy in range(9):
-                if sudoku[ix][iy] != 0:
-                    tblItem = QTableWidgetItem(str(sudoku[ix][iy]))
-                    tblItem.setTextAlignment(Qt.AlignHCenter | Qt.AlignVCenter | Qt.AlignCenter)
-                    self.tblSudoku.setItem(ix, iy, tblItem)
-                    QApplication.processEvents()
-                else:
-                    tblItem = QTableWidgetItem("")
-                    tblItem.setTextAlignment(Qt.AlignHCenter | Qt.AlignVCenter | Qt.AlignCenter)
-                    self.tblSudoku.setItem(ix, iy, tblItem)
-                    QApplication.processEvents()
-        # Show Widgets
-        layout.addWidget(self.tblSudoku)
-        layout.addWidget(self.btnRun)
-        layout.addWidget(self.btnFastRun)
-        self.setLayout(layout)
-
-    def runSolver(self, runType):
-        global btEnd, sudoku, fastMode
-        self.btnRun.setEnabled(False)
-        self.btnFastRun.setEnabled(False)
-        if runType == 0:
-            self.btnFastRun.setText("N/A")
-            self.btnRun.setText(translates[lang]["calcText"])
-        else:
-            self.btnRun.setText("N/A")
-            self.btnFastRun.setText(translates[lang]["fastModeText"])
-            fastMode = True
-        for ax in range(9):
-            for ay in range(9):
-                gotItem = self.tblSudoku.item(ax, ay).text()
-                if gotItem != "":
-                    try:
-                        sudoku[ax][ay] = int(self.tblSudoku.item(ax, ay).text())
-                    except:
-                        self.btnRun.setText(translates[lang]["invalidItemsText"])
-                        self.btnFastRun.setText(translates[lang]["invalidItemsText"])
-
-                else:
-                    sudoku[ax][ay] = 0
-        # noinspection PyTypeChecker
-        self.tblSudoku.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        QApplication.processEvents()
-        subThread = calculate()
-        subThread.updTblSignal.connect(updTable)
-        subThread.updSttSignal.connect(updStatus)
-        subThread.basic()
-        subThread.start()
-        while not btEnd:
-            QApplication.processEvents()
-
 
 # noinspection PyUnresolvedReferences
-class calculate(PyQt6.QtCore.QThread):
-    updTblSignal = PyQt6.QtCore.pyqtSignal(int, int, int)
-    updSttSignal = PyQt6.QtCore.pyqtSignal()
-
+class calculate:
     def __init__(self):
         super().__init__()
 
@@ -154,7 +58,6 @@ class calculate(PyQt6.QtCore.QThread):
                 if calcPos(xP, yP, testNum, False) is False:
                     continue
                 sudoku[xP][yP] = testNum
-                if not fastMode: self.updTblSignal.emit(xP, yP, abs(testNum))
                 if not fastMode: time.sleep(0.00000000000000000000000000000000000000000000000001)
                 if xP == 8 and yP == 8:
                     return self.showRight()
@@ -175,7 +78,6 @@ class calculate(PyQt6.QtCore.QThread):
                     self.stepGrid("n")
                 else:
                     sudoku[xP][yP] = 0
-                    if not fastMode: self.updTblSignal.emit(xP, yP, 0)
             else:
                 if xP != 8:
                     xP += 1
@@ -184,7 +86,6 @@ class calculate(PyQt6.QtCore.QThread):
                         self.stepGrid("n")
                     else:
                         sudoku[xP][yP] = 0
-                        if not fastMode: self.updTblSignal.emit(xP, yP, 0)
                 else:
                     return self.showRight()
         elif stepType == "b":
@@ -194,7 +95,6 @@ class calculate(PyQt6.QtCore.QThread):
                     self.stepGrid("b")
                 else:
                     sudoku[xP][yP] = 0
-                    if not fastMode: self.updTblSignal.emit(xP, yP, 0)
             else:
                 if xP != 0:
                     xP -= 1
@@ -203,16 +103,13 @@ class calculate(PyQt6.QtCore.QThread):
                         self.stepGrid("b")
                     else:
                         sudoku[xP][yP] = 0
-                        if not fastMode: self.updTblSignal.emit(xP, yP, 0)
 
     def showRight(self):
         global sudoku, stepTime, btEnd, fastMode
         for fx in range(9):
             for fy in range(9):
                 sudoku[fx][fy] = abs(sudoku[fx][fy])
-                if fastMode: self.updTblSignal.emit(fx, fy, abs(sudoku[fx][fy]))
         btEnd = True
-        self.updSttSignal.emit()
         return True
 
     def basic(self):
@@ -224,8 +121,6 @@ class calculate(PyQt6.QtCore.QThread):
                         calcPos(x, y)
                         if len(possibilities) == 1:
                             sudoku[x][y] = int(possibilities)
-                            if not fastMode: self.updTblSignal.emit(x, y, int(possibilities))
-                            QApplication.processEvents()
                             solutedNumbers += 1
                     possibilities = "123456789"
             if solutedNumbers == 0:
@@ -239,33 +134,3 @@ class calculate(PyQt6.QtCore.QThread):
                     if sudoku[n][p] == 0:
                         stopBasic = False
 
-
-if __name__ == "__main__":
-    if getdefaultlocale()[0] == "zh_CN":
-        lang = "zh"
-    else:
-        lang = "en"
-    app = QApplication(sys.argv)
-    widget = SSGui()
-    widget.show()
-
-
-    def updTable(uX, uY, uNum):
-        global widget
-        if uNum != 0:
-            tblItem = QTableWidgetItem(str(uNum))
-            tblItem.setTextAlignment(Qt.AlignHCenter | Qt.AlignVCenter | Qt.AlignCenter)
-            tblItem.setForeground(QBrush(QColor(139, 129, 76)))
-        else:
-            tblItem = QTableWidgetItem("")
-            tblItem.setTextAlignment(Qt.AlignHCenter | Qt.AlignVCenter | Qt.AlignCenter)
-        widget.tblSudoku.setItem(uX, uY, tblItem)
-
-
-    def updStatus():
-        global lang, widget
-        widget.btnRun.setText(translates[lang]["calcEndText"])
-        widget.btnFastRun.setText(translates[lang]["calcEndText"])
-
-
-    sys.exit(app.exec())
